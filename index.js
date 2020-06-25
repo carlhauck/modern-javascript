e342862ef021de3251bfcf125a111d610abdbcc9
carlhauck <carlhauck@gmail.com>
2020-06-25 14:20:06 -0500

    adds math.js, webpack, and webpack-dev-server

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 /* global moment */
 
-console.log("Hello from JavaScript!");
+import moment from 'moment';
+import { sqrt } from 'mathjs/number';
+
+console.log("JavaScript doesn't care either!");
 var bday = moment("19870812", "YYYYMMDD").fromNow();
 console.log(bday);
+console.log(Number(sqrt(144)));
